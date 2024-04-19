2003dd4d20f1646d1e30e3d1e754ca4d84bd6eab
Kaspar Kohler <kaspar.kohler@nortal.com>
2024-04-19 22:58:54 +0300

    add option to run multiple environments

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,9 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://www.lhv.ee/et',
+    baseURL: process.env.NODE_ENV === 'live'
+      ? 'https://www.lhv.ee/et'
+      : 'https://www.lhv.ee/et', // Default base URL for local development
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
