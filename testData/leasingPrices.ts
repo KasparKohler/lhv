1f8ff7c3c6bec289b69cfb4362d093556678015e
Kaspar Kohler <kaspar.kohler@nortal.com>
2024-04-19 16:43:20 +0300

    add test data for leasing calculator

---
+++ testData/leasingPrices.ts
@@ -0,0 +1,11 @@
+export default class LeasingPrices {
+    samples = {
+        "default": {
+            "totalPrice": 25000,
+            "InitialPaymentAmount": 2500,
+            "InitialPaymentPercentage": 11
+        }
+    }
+
+    individualLeasingExample = this.samples.default
+}
